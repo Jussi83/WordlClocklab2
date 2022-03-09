f2abb93aa0a591e4c6f7009b97c01df2fe756cc7
Jussi <martin.jussi83@hotmail.se>
2022-03-09 12:49:54 +0100

    added some functional code for clocks to run

---
+++ main.js
@@ -0,0 +1,122 @@
+const cityHeader = document.getElementById('current-city');
+const selectCity = document.getElementById('select-city');
+const cityListElement = document.getElementsByName('city-list')[0];
+
+let citysList = [];
+let thisTimeZone = 'Europe/Paris';
+let timeZones = [];
+
+function setTime() {
+  //update the current time every second
+  setInterval(() => {
+    let date = new Date();
+    let options = {
+      timeZone: `${thisTimeZone}`,
+      hour12: false,
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    }
+    let currentTimeDigital = new Intl.DateTimeFormat('en-US', options).format(date);
+    digitalTime.textContent = currentTimeDigital;
+
+    options = {
+      timeZone: `${thisTimeZone}`,
+      hour12: false,
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      fractionalSecondDigits: 3
+    }
+
+    let currentTimeAnalog = new Intl.DateTimeFormat('en-US', options).format(date);
+
+    let hr = currentTimeAnalog.split(':')[0];
+    let min = currentTimeAnalog.split(':')[1];
+    let sec = currentTimeAnalog.split(':')[2];
+
+    hr_rotation = 30 * hr + min / 2;
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+
+
+  }, 1);
+}
+
+fillDatalist();
+setTime();
+async function fillDatalist() {
+  let rawData = await fetch('timezones.json');
+  country = await rawData.json();
+
+  for (let i = 0; i < country.length; i++) {
+    let name = country[i].WindowsTimeZones[0].Name.split(') ')[1];
+    let timeZone = country[i].TimeZones[0];
+    timeZones.push(timeZone)
+    let city = {
+      name: name,
+      timeZone: timeZone
+    }
+    citylist.push(city);
+  }
+
+  for (city of citylist) {
+    let tempNamesArray = new Array
+    let arrayTemp = new Array
+    //if one entry contains several cities, split into several entries
+    if (city.name.includes(',')) {
+      tempNamesArray = city.name.split(', ');
+
+      for (let i = 0; i < tempNamesArray.length; i++) {
+        let thisCity = {
+          name: tempNamesArray[i],
+          timeZone: city.timeZone
+        }
+        arrayTemp.push(thisCity)
+      }
+
+      citylist = citylist.concat(arrayTemp);
+      const index = citylist.indexOf(city);
+      citylist.splice(index, 1);
+    }
+  }
+
+  let mycitys;
+  if (JSON.parse(localStorage.getItem("myCitys")) !== null) {
+    mycitys = JSON.parse(localStorage.getItem("myCitys"));
+    citylist = citylist.concat(mycitys);
+  }
+
+  //sorts citys in alphabetically order
+  citylist.sort((a, b) => {
+    let fa = a.name.toLowerCase(),
+      fb = b.name.toLowerCase();
+
+    if (fa < fb) {
+      return -1;
+    }
+    if (fa > fb) {
+      return 1;
+    }
+    return 0;
+  });
+
+  //removes duplication
+  citylist = Array.from(citylist.reduce((a, o) => a.set(o.name, o), new Map()).values());
+
+  let html = '';
+
+  for (city of citylist) {
+    html += '<option>' + city.name + '</option>'
+  }
+
+  //fills datalist with citys
+  selectCity.innerHTML = html;
+
+  timeZones.sort()
+  fillInputTimezone();
+}
